34ef9645e9f6b80266ab749da754031e4b49d341
Umang Chovatiya <umang.chovatiya@yudizsolutions.com>
2024-10-10 10:35:47 +0530

    Data Binding completed

--- app/src/main/java/com/example/android_app_demo/data_binding/DataBinding.kt
+++ app/src/main/java/com/example/android_app_demo/data_binding/DataBinding.kt
@@ -1,14 +1,13 @@
 package com.example.android_app_demo.data_binding
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.android_app_demo.R
 import com.example.android_app_demo.databinding.ActivityDataBindingBinding
-import java.util.Date
+import java.time.LocalDate
 
 class DataBinding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +16,11 @@ class DataBinding : AppCompatActivity() {
         val binding: ActivityDataBindingBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
 
-        binding.user =
-            ContextCompat.getDrawable(this, R.drawable.avatar)?.let {
-                MyUser(
-                    "Umang",
-                    "Chovatiya",
-                    it,
-                    Date(2002, 10, 25)
-                )
-            }
-        Log.d("date", Date(2002, 10, 25).toString())
+        val date = LocalDate.of(2002, 10, 25)
+        //val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date)
+
+        ContextCompat.getDrawable(this, R.drawable.avatar)?.let {
+            binding.user = MyUser("Umang", "Chovatiya", it, date)
+        }
     }
 }
--- app/src/main/java/com/example/android_app_demo/data_binding/MyUser.kt
+++ app/src/main/java/com/example/android_app_demo/data_binding/MyUser.kt
@@ -3,30 +3,18 @@ package com.example.android_app_demo.data_binding
 import android.graphics.drawable.Drawable
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.Period
-import java.util.Date
-import java.util.Locale
 
 data class MyUser(
     val name: String,
     val lastName: String,
     val img: Drawable,
-    val birthDate: Date
+    val birthDate: LocalDate
 ) {
-    fun calculateBirthDate(view: View, birthDate: Date) {
-        val date = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(birthDate)
-        if (date != null) {
-            val years = getAge(date.year, date.month, date.day)
-            Snackbar.make(view, "you are currently $years old", Snackbar.LENGTH_LONG).show()
-        }
-    }
-
-    fun getAge(year: Int, month: Int, dayOfMonth: Int): Int {
-        return Period.between(
-            LocalDate.of(year, month, dayOfMonth),
-            LocalDate.now()
-        ).years
+    fun calculateAge(view: View) {
+        val currentDate = LocalDate.now()
+        val age = Period.between(birthDate, currentDate).years
+        Snackbar.make(view, "You are currently $age years old", Snackbar.LENGTH_LONG).show()
     }
 }
